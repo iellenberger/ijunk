90c613d57bd321d1c43a9d39ca557d7ab11bcaed
Ingmar Ellenberger <ingmar@site42.com>
2014-07-03 20:55:19 -0700

    Adding fragment for text interpolation and hash flattening

---
+++ src/fragments/perl/interpolate.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/perl -w
+
+use Data::Dumper; $Data::Dumper::Indent=$Data::Dumper::Sortkeys=$Data::Dumper::Terse=1; # for debugging only
+
+use feature qw( switch );
+
+use strict;
+use warnings;
+
+# --- text for interpolation ---
+my $text = '
+	This is a ${key1} of the ${hash1:key2} ${hash1:hash2:key3} system
+	This is not how to count: ${array:0} ${array:1} ${array:2} ${array:3} ${array:4}
+';
+# --- datastruct of values ---
+my $hash = {
+	key1 => 'test',
+	hash1 => {
+		key2 => 'variable',
+		hash2 => {
+			key3 => 'interpolation',
+		},
+	},
+	array => [ qw( one tow 3 for fyve ) ],
+		
+};
+
+# --- show results ---
+print "Original Hash: ". Dumper($hash) ."\n";
+my $flat = flatten($hash);
+print "Flattened Hash: ". Dumper($flat) ."\n";
+print "Original Text:\n". $text ."\n";
+print "Interpolated Text:\n". interpolate($text, $flat) ."\n";
+
+# --- interpolate hash into text ---
+sub interpolate {
+	my ($text, $hash) = @_;
+	while (my ($key, $value) = each %$hash) {
+		$text =~ s/\${$key}/$value/;
+	}
+	return $text;
+}
+
+# --- flatten a datastructure ---
+sub flatten {
+	my ($ds, $prefix) = @_;
+	my $flat = {};
+
+	given (ref $ds) {
+		when ('HASH') {
+			foreach my $key (keys %$ds) {
+				my $subhash = flatten($ds->{$key}, defined $prefix ? "$prefix.$key" : $key);
+				map { $flat->{$_} = $subhash->{$_} } keys %$subhash;
+		}	}
+		when ('ARRAY') {
+			for (my $key = 0; $key < @$ds; $key++) {
+				my $subhash = flatten($ds->[$key], defined $prefix ? "$prefix:$key" : $key);
+				map { $flat->{$_} = $subhash->{$_} } keys %$subhash;
+		}	}
+		default { $flat = { $prefix => $ds } }
+	}
+
+	return $flat;
+}
