0bd1c0c67ad18ea60680372d44df8b8ffce71577
Ingmar Ellenberger <iee+github@site42.com>
2014-08-11 13:58:30 -0700

    Adding functional interface for sourceshell

--- src/fragments/perl/sourceshell.pl
+++ src/fragments/perl/sourceshell.pl
@@ -99,3 +99,85 @@ print "\n";
 print "pl post : FOO is ". (defined $ENV{FOO} ? "'$ENV{FOO}'" : "undef") ."\n";
 print "pl post : BAR is ". (defined $ENV{BAR} ? "'$ENV{BAR}'" : "undef") ."\n";
 print "pl post : BAZ is ". (defined $ENV{BAZ} ? "'$ENV{BAZ}'" : "undef") ."\n";
+
+exit;
+
+# === Functions to make things easier =======================================
+# The code below is not actually used in the examples above.  It is provided
+# as copy-and-paste options for your own code.  Paste the code (including the
+# 'use' statements) and you can source your uoen shell scripts without having
+# to modify them first.
+#
+#   source('content') or
+#   sourcefile('filename')
+
+use English;
+use FindBin qw( $RealScript );
+use Storable qw( retrieve );
+
+# --- 'source' the content of a Bash script ---
+sub source {
+	# --- script content - NOT a filename ---
+	my $content = shift;
+	# --- get/create tmpfile name ---
+	my $tmpfile = shift || ($ENV{TMP_DIR} || '/tmp') ."/$RealScript.source.$PID";
+
+	# --- write the script content ---
+	open TMPFILE, ">$tmpfile"
+		or die "script(): unable to write file '$tmpfile': $!";
+	print TMPFILE $content;
+	close TMPFILE;
+
+	# --- source the file ---
+	sourcefile($tmpfile);
+
+	# --- remove the file ---
+	unlink $tmpfile;
+}
+
+# --- 'source' a Bash script ---
+sub sourcefile {
+	# --- script filename ---
+	my $script = shift;
+	# --- get/create tmpfile name ---
+	my $tmpfile = shift || ($ENV{TMP_DIR} || '/tmp') ."/$RealScript.source.$PID";
+
+	# --- capture the current ENV ---
+	my $env = \%ENV;
+
+	# --- run the script ---
+	system qq[
+		set -a
+		source $script
+		perl -e 'use Storable; store \\\%ENV, "$tmpfile"'
+	];
+
+	# --- throw an error if things didn't get sourced properly ---
+	unless (-e $tmpfile) {
+		print STDERR "
+error sourcing shell script '$tmpfile;
+
+If you're seeing this message, it's likely the file you are trying to source
+has an 'exit' command in it that's messing things up.  The 'exit' command is
+not allowed outside of functions when trying to source a shell script.
+
+";
+		die "tmpfile '$tmpfile' does not exist";
+	}
+
+	# --- retrieve the new env hash and delete the tempfile ---
+	my $newenv = retrieve $tmpfile;
+	unlink $tmpfile;
+
+	# --- import new/changed envvars ---
+	my $ignore = join '|', qw( _ PWD SHLVL USER );  # ignore certain values
+
+	# --- copy new/changed values ---
+	foreach my $key (keys %$newenv) {
+		next if $key =~ /^(?:$ignore)$/;
+		$env->{$key} = $newenv->{$key}
+			if !exists $ENV{$key} || !defined $ENV{$key} || $ENV{$key} ne $newenv->{$key};
+	}
+
+	return $env;
+}
