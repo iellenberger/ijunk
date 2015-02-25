c2946b0a46172f09deb0f904f66d8e083ff1ba09
Ingmar Ellenberger <ingmar@site42.com>
2015-02-24 16:11:53 -0800

    More forms of sudoit

--- src/fragments/bash/sudoit.sh
+++ src/fragments/bash/sudoit.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/bin/bash -x
 
 # --- sudo one-liner ---
 if [ "$EUID" != 0 ]; then sudo $0 $*; exit; fi
 
 # --- do something ---
 whoami
+exit;
+
+# === Other Examples ========================================================
+
+# --- as explicit user ---
+if [ "$USER" != "root" ]; then sudo -u root $0 $*; exit; fi
+
+# --- as explicit user w/ login shell ---
+if [ "$USER" != "root" ]; then sudo -i -u root $(cd $(dirname $0); pwd)/$(basename $0) $*; exit; fi
