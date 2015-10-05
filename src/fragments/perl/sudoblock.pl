22c69d2d488b289b882ac8e4c8e5cddbe42e4d54
Ingmar Ellenberger <iee+github@site42.com>
2015-10-05 08:45:35 -0700

    Example of how to run multiline block under sudo

---
+++ src/fragments/perl/sudoblock.pl
@@ -0,0 +1,27 @@
+#!/usr/bin/perl -w
+
+use English;
+
+use strict;
+use warnings;
+
+sudo("
+	echo running as \$USER 
+	sleep 1
+");
+
+# --- execute multiline bash as superuser ---
+sub sudo {
+	my $block = shift;
+	my $tmpfile = "/tmp/sudoblock.$$.tmp";
+
+	open TMPFILE, ">$tmpfile" || die;
+	print TMPFILE $block;
+	close TMPFILE;
+
+	my $cmd = "bash $tmpfile";
+	$cmd = "sudo $cmd" if $UID != 0;
+	system $cmd;
+	unlink $tmpfile;
+}
+
