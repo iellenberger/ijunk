c1c83713e602916db08f8830fcefae5e341437bf
Ingmar Ellenberger <iee+github@site42.com>
2017-08-17 11:55:10 -0700

    Added error handling and cleaned up formatting

--- src/fragments/bash/boolean.sh
+++ src/fragments/bash/boolean.sh
@@ -8,31 +8,42 @@ testbool() {
 
 	# --- test if it's set ---
 	if [ ! -z ${MYBOOL+x} ]; then
-		echo '   ! -z ${MYBOOL+x}  : true'
+		echo '   ! -z ${MYBOOL+x} : true'
 	else
-		echo '   ! -z ${MYBOOL+x}  : false'
+		echo '   ! -z ${MYBOOL+x} :       false'
 	fi
 
-	# --- test if it evaluates ---
-	if [ $MYBOOL ] ; then
-		echo '   $MYBOOL           : true'
+	# --- test if it evaluates without quotes ---
+	if [ $MYBOOL 2> /dev/null ] ; then
+		echo '   $MYBOOL          : true'
+	elif [ "$?" == 2 ]; then
+		echo '   $MYBOOL          :              error'
 	else
-		echo '   $MYBOOL           : false'
+		echo '   $MYBOOL          :       false'
+	fi
+
+	# --- test if it evaluates with quotes ---
+	if [ "$MYBOOL" ] ; then
+		echo '   "$MYBOOL"        : true'
+	else
+		echo '   "$MYBOOL"        :       false'
 	fi
 
 	# --- test if it evaluates to true without quotes ---
-	if [ $MYBOOL = true ] ; then
-		echo '   $MYBOOL = true    : true'
+	if [ $MYBOOL = true 2> /dev/null ] ; then
+		echo '   $MYBOOL = true   : true'
+	elif [ "$?" == 2 ]; then
+		echo '   $MYBOOL = true   :              error'
 	else
-		echo '   $MYBOOL = true    : false'
+		echo '   $MYBOOL = true   :       false'
 	fi
 
 	# --- test if it evaluates to true with quotes ---
 	# NOTE: this seems the most likely solution you're looking for
 	if [ "$MYBOOL" = true ] ; then
-		echo '   "$MYBOOL" = true  : true'
+		echo '   "$MYBOOL" = true : true'
 	else
-		echo '   "$MYBOOL" = true  : false'
+		echo '   "$MYBOOL" = true :       false'
 	fi
 
 	echo
