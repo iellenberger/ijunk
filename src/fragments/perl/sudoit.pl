2cea8ccd56960a86f515d486cd029efde1c22cbf
Ingmar Ellenberger <iee+github@site42.com>
2021-12-20 13:34:57 -0800

    Improving the way @ARGV is handled

--- src/fragments/perl/sudoit.pl
+++ src/fragments/perl/sudoit.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl -w
 
 # --- one-liner method ---
-$> && exec "sudo $0 ". join(' ', @ARGV); # always run as superuser
+$> && exec "sudo", $0, @ARGV; # always run as superuser
 
 use English;
 
@@ -15,13 +15,13 @@ my @args = @ARGV;
 
 # add conditions for sudo call here
 if ($UID != 0) {
-	my $cmd = "sudo $0 ". join(' ', @args);
-	exec $cmd;
+	my @cmd = ("sudo", $0, @ARGV);
+	exec @cmd;
 }
 
 exec 'whoami';
 
 # --- an example of being a particular user ---
-# using infact(1058) for this example
-$> != 1058 and exec "sudo -i -u infact $0 ". join(' ', @ARGV); # via uid
-$ENV{USER} != 'infact' and exec "sudo -i -u infact $0 ". join(' ', @ARGV); # via username
+# using jimbob(1058) for this example
+$> != 1058 and exec qw( sudo -i -u 1058 ), $0, @ARGV; # via uid
+$ENV{USER} != 'jimbob' and exec qw( sudo -i -u jimbob ), $0, @ARGV; # via username
